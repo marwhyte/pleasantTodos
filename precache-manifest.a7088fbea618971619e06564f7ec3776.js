d0f828a007ab47f2530c9b55a7a385a7917cffbb
Marco Whyte <mwhyte@umass.edu>
2020-05-17 13:35:16 -0500

    Updates

---
+++ precache-manifest.a7088fbea618971619e06564f7ec3776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e373bfc2d00cc579235108232679e338",
+    "url": "/pleasantTodos/index.html"
+  },
+  {
+    "revision": "4776ca9106cf5bed1dd1",
+    "url": "/pleasantTodos/static/css/main.d687ab15.chunk.css"
+  },
+  {
+    "revision": "948fe9c947f21b0779bf",
+    "url": "/pleasantTodos/static/js/2.84f9f1f7.chunk.js"
+  },
+  {
+    "revision": "9f53b0b4cf49772fd2c6a4f60743c42f",
+    "url": "/pleasantTodos/static/js/2.84f9f1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4776ca9106cf5bed1dd1",
+    "url": "/pleasantTodos/static/js/main.9e347af1.chunk.js"
+  },
+  {
+    "revision": "04df15ddc3512f30966c",
+    "url": "/pleasantTodos/static/js/runtime-main.30d706ce.js"
+  }
+]);
